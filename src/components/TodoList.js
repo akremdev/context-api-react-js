546831a3fca335f941033eb56c5a03290f35ea2f
akremdev <akremdev@gmail.com>
2021-04-24 18:21:21 +0100

    add new todo with hooks + context

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 import { TodoListContext } from '../contexts/TodoListContext';
 
@@ -19,11 +19,21 @@ import { TodoListContext } from '../contexts/TodoListContext';
 } */
 
 const TodoList = () => {
-    const {todos} = useContext(TodoListContext);
+    const [todo, setTodo] = useState('')
+    const {todos, addTodo} = useContext(TodoListContext);
     const {isDarkTheme, darkTheme, lightTheme, changeTheme} = useContext(ThemeContext);
     const theme = isDarkTheme ? darkTheme : lightTheme;
+
+    const handleChange = (e) => {
+        setTodo(e.target.value);
+    }
+
+    const handleFormSubmit= (e) => {
+        e.preventDefault();
+        addTodo(todo);
+    }
     return (
-            <div style={{background:theme.background, color:theme.text, height:'140px', textAlign:'center'}} > 
+            <div style={{background:theme.background, color:theme.text, textAlign:'center'}} > 
             {
                 todos.length ? (
                     todos.map((todo) => {
@@ -38,6 +48,11 @@ const TodoList = () => {
                 {/* <p className="item">Plan the family trip</p>
                 <p className="item">Go for a shopping</p>
                 <p className="item">Go for a walk</p> */}
+                <form onSubmit={handleFormSubmit}>
+                    <label htmlFor="todo">Add todo:</label>
+                    <input type="text" id='todo' onChange={handleChange}/>
+                    <input type="submit" value="add new todo" className="ui button primary"/>
+                </form>
                 <button className="ui button primary" onClick={changeTheme}>change the theme</button>
             </div>  
     );
