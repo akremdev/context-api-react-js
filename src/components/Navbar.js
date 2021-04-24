5f3328b0cad30a4566105801249b3a374c2de357
akremdev <akremdev@gmail.com>
2021-04-24 02:03:40 +0100

    context consumer method

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,21 +2,27 @@ import React from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends React.Component {
-    static contextType = ThemeContext;
+    //static contextType = ThemeContext;
+    // context consumer is used in class and functions components
     render() {
-        const {isDarkTheme, darkTheme, lightTheme} = this.context;
-        const theme = isDarkTheme ? darkTheme : lightTheme;
         return (
-            <nav style={{ background:theme.background , color:theme.text, height: '120px' }}>
-                <h2 style={{textAlign: 'center' }}>
-                Akrem Academy
-                </h2>
-                <div className="ui three buttons">
-                    <button className="ui button">Overview</button>
-                    <button className="ui button">Contact</button>
-                    <button className="ui button">Support</button>
-                </div>
-            </nav>
+            <ThemeContext.Consumer>{(context) => {
+                const {isDarkTheme, darkTheme, lightTheme} = context;
+                const theme = isDarkTheme ? darkTheme : lightTheme;
+                return(
+                    <nav style={{ background:theme.background , color:theme.text, height: '120px' }}>
+                    <h2 style={{textAlign: 'center' }}>
+                    Akrem Academy
+                    </h2>
+                    <div className="ui three buttons">
+                        <button className="ui button">Overview</button>
+                        <button className="ui button">Contact</button>
+                        <button className="ui button">Support</button>
+                    </div>
+                </nav>
+                    )
+            }}
+            </ThemeContext.Consumer>
         )
     }
 }
