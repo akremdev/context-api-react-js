8d7747cea7ce808c65df7aa84d525f3a201a520f
akremdev <akremdev@gmail.com>
2021-04-24 23:53:42 +0100

    make it better v2

--- src/contexts/TodoListContext.js
+++ src/contexts/TodoListContext.js
@@ -1,21 +1,9 @@
-import React, {createContext, useState, useReducer} from 'react';
+import React, {createContext, /* useState, */ useReducer} from 'react';
+import {todosReducer} from '../reducers/todosReducer';
 
 export const TodoListContext = createContext();
 
-const todosReducer = (state, action) => {
-    switch (action.type) {
-        case 'ADD_TODO':
-            return [
-                ...state,
-                {text : action.text , id: Math.random()}
-            ]
-           
-        case 'REMOVE_TODO':
-                return state.filter((todo) => todo.id !== Number(action.id));
-        default:
-                return state;
-    }
-}
+
 
 const TodoListContextProvider = ({children}) => {
     const [todos, dispatch] = useReducer(todosReducer, [
