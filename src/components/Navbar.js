58e93df0da64ec9ab66318453ac30c04031f8da1
akremdev <akremdev@gmail.com>
2021-04-24 17:22:48 +0100

    create multiple context with useContext

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 import { AuthContext } from '../contexts/AuthContext';
 
-class Navbar extends React.Component {
+/* class Navbar extends React.Component {
     //static contextType = ThemeContext;
     // context consumer is used in class and functions components
     render() {
@@ -30,11 +30,30 @@ class Navbar extends React.Component {
                     </ThemeContext.Consumer>
                 )
             }}
-
             </AuthContext.Consumer>
-            
         )
     }
+} */
+
+const Navbar = () => {
+    const {isDarkTheme, darkTheme, lightTheme} = useContext(ThemeContext);
+    const {isLoggedIn, changeAuthStatus} = useContext(AuthContext);
+    const theme = isDarkTheme ? darkTheme : lightTheme;
+    return(
+        <nav style={{ background:theme.background , color:theme.text, height: '120px' }}>
+            <h2 style={{textAlign: 'center' }}>
+                Akrem Academy
+            </h2>
+                <p onClick={changeAuthStatus} style={{ textAlign:'center'}}>{ isLoggedIn ? 'logged in ' : 'logged out' }
+                </p>
+                <div className="ui three buttons">
+                    <button className="ui button">Overview</button>
+                    <button className="ui button">Contact</button>
+                    <button className="ui button">Support</button>
+                </div>
+        </nav>
+    );
 }
+
 
 export default Navbar;
